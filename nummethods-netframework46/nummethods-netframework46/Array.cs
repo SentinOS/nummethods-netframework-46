29786d56ff869cbb9eb1cb131b9404b6c7bdfa58
EXZ <EXZ@DESKTOP-PQAGO3V>
2019-03-18 21:28:57 +0400

    -Добавлен Метод СЛАУ -Изменены названия переменных-массивов -Изменен тип Dimension с uint на int

--- nummethods-netframework46/nummethods-netframework46/Array.cs
+++ nummethods-netframework46/nummethods-netframework46/Array.cs
@@ -5,42 +5,52 @@ namespace NumMethods
     public class Array
     {
 
-        private double[,] A;
+        private double[,] MtrxOfCoefs;
+
 
         public bool AFactorized { get; set; }
 
-        private double[] B;
+        private double[] VctrOfFreeMembers;
 
-        private double[] y;
+        private double[] VctrOfVars;
 
-        public uint Dimension { get; set; }
+        public int Dimension { get; set; }
 
 
         public void InsertData()
         {
             Console.Clear();
 
-            Console.WriteLine("Введите размерность матрицы A:");
-            Dimension = uint.Parse(Console.ReadLine());
-            this.A = new double[Dimension, Dimension];
-            Console.WriteLine("Введите элементы матрицы A:\n");
+            Console.WriteLine("Введите размерность матрицы А:");
+            Dimension = int.Parse(Console.ReadLine());
+            if(Dimension<=0)
+            {
+                Console.WriteLine("Введите размерность матрицы А заново.\nНедопустимое значение.");
+                Console.ReadKey();
+                Console.Clear();
+                return;
+            }
+            this.MtrxOfCoefs = new double[Dimension, Dimension];
+        
+            Console.WriteLine("Введите элементы матрицы А:\n");
             for (int i = 0; i < Dimension; i++)
                 for (int j = 0; j < Dimension; j++)
                 {
                     try
                     {
-                        A[i, j] = double.Parse(Console.ReadLine());
+                        MtrxOfCoefs[i, j] = double.Parse(Console.ReadLine());
                     }
 
                     catch
                     {
                         Console.WriteLine("Неправильно введен элемент. Введите заново!");
-                        A[i, j] = double.Parse(Console.ReadLine());
+                        MtrxOfCoefs[i, j] = double.Parse(Console.ReadLine());
                     }
 
                 }
 
             Console.Clear();
+            this.VctrOfVars = new double[Dimension];
             AFactorized = false;
             PrintData();
             Console.Clear();
@@ -50,11 +60,11 @@ namespace NumMethods
         {
             Console.Clear();
 
-            Console.WriteLine("Матрица A:\n");
+            Console.WriteLine("Матрица А:\n");
             for (int i = 0; i < Dimension; i++)
             {
                 for (int j = 0; j < Dimension; j++)
-                    Console.Write(A[i, j] + " \t");
+                    Console.Write(MtrxOfCoefs[i, j] + " \t");
                 Console.WriteLine();
             }
 
@@ -69,10 +79,10 @@ namespace NumMethods
             for (int k = 0; k < Dimension; k++)
             {
                 for (int j = k + 1; j < Dimension; j++)
-                    A[k, j] /= A[k, k];
+                    MtrxOfCoefs[k, j] /= MtrxOfCoefs[k, k];
                 for (int i = k + 1; i < Dimension; i++)
                     for (int j = k + 1; j < Dimension; j++)
-                        A[i, j] -= A[i, k] * A[k, j];
+                        MtrxOfCoefs[i, j] -= MtrxOfCoefs[i, k] * MtrxOfCoefs[k, j];
             }
 
 
@@ -104,35 +114,50 @@ namespace NumMethods
         public void SolutionSLAE()
         {
             Console.Clear();
-            Console.WriteLine("Введите матрицу B: \n");
-            this.B = new double[Dimension];
-            for (int i = 0; i < Dimension; i++)
+
+            if (AFactorized)
             {
-                try
-                {
-                    B[i] = double.Parse(Console.ReadLine());
-                }
+                    Console.WriteLine("Введите вектор свободных членов: \n");
+                    this.VctrOfFreeMembers = new double[Dimension];
+                    for (int i = 0; i < Dimension; i++)
+                    {
+                        try
+                        {
+                            VctrOfFreeMembers[i] = double.Parse(Console.ReadLine());
+                        }
 
-                catch
-                {
-                    Console.WriteLine("Неправильно введен элемент. Введите заново!");
-                    B[i] = double.Parse(Console.ReadLine());
-                }
+                        catch
+                        {
+                            Console.WriteLine("Неправильно введен элемент. Введите заново!");
+                            VctrOfFreeMembers[i] = double.Parse(Console.ReadLine());
+                        }
 
+                    }
+
+                
+                    double TempSum = 0;
+
+                    for (int i = Dimension-1; i>=0; i--)
+                    {
+                        TempSum = 0;
+                        for (int j=Dimension-1; j> i;j--)
+                        {
+                            TempSum += MtrxOfCoefs[i, j] * VctrOfVars[j];
+                        }
+
+                        VctrOfVars[i] = VctrOfFreeMembers[i]/MtrxOfCoefs[i,i] - TempSum;
+                    }
+
+                    Console.WriteLine("Вектор неизвестных X:\n");
+                    for (uint i = 0; i < Dimension - 1; i++)
+                        Console.WriteLine(VctrOfVars[i] + "\n");
+            }   
+
+            else
+            {
+                    Console.WriteLine("Матрица не факторизована!");
             }
 
-            for (int i = 0; i < Dimension; i++)
-            {
-                double sum = 0;
-                for (int j = 0; j < i; j++)
-                    sum += A[i, j] * y[j];
-                y[i] = (B[i] - sum) / A[i, i];
-            }
-
-            for (int i = (int)Dimension - 1; i > -1; i--)
-            {
-                double sum = 0;
-            }
             Console.ReadKey();
             Console.Clear();
         }
@@ -142,9 +167,9 @@ namespace NumMethods
             Console.Clear();
             if (AFactorized)
             {
-                double determinant = A[0, 0];
+                double determinant = MtrxOfCoefs[0, 0];
                 for (int i = 1; i < Dimension; i++)
-                    determinant *= A[i, i];
+                    determinant *= MtrxOfCoefs[i, i];
 
                 Console.WriteLine("Детерминант матрицы равен {0}", determinant);
             }
